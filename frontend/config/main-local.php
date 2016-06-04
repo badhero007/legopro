f0970b5406e59a2fcd90d05c6a562a4aac39dff1
376762626@qq.com <021271>
2016-06-04 11:56:20 +0800

    add localconfig file

---
+++ frontend/config/main-local.php
@@ -0,0 +1,22 @@
+<?php
+
+$config = [
+
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'l_6e11JpSa8ww6cEjeybz3_nJ3P0JjCP',
+        ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = 'yii\debug\Module';
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = 'yii\gii\Module';
+}
+
+return $config;
