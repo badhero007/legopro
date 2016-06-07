2d4e3144b2b530bc91fd8b253aa9dcd012a63122
376762626@qq.com <021271>
2016-06-07 10:54:27 +0800

    modify mongo port

--- common/config/params.php
+++ common/config/params.php
@@ -6,7 +6,7 @@ return [
     'mongodb' => [
         'common' => [
             'host' => '127.0.0.1',
-            'port' => '3717',
+            'port' => '27017',
             'connect' => true,
             'timeout' => 1000,
             'username' => 'lego',
