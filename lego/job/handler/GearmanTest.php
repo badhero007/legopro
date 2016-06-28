f370296dbca5b71eae26ae72ae6e3778950036c3
376762626@qq.com <021271>
2016-06-28 17:29:35 +0800

    testgearman

--- lego/job/handler/GearmanTest.php
+++ lego/job/handler/GearmanTest.php
@@ -7,13 +7,14 @@
  */
 namespace lego\job\handler;
 
+use common\core\Llog;
 use shakura\yii2\gearman\JobBase;
 
 class GearmanTest extends JobBase {
     public function execute(\GearmanJob $job = null){
         $loader = $this->getWorkload($job);
         $params = $loader->getParams();
-
+        Llog::log('test','test');
         var_dump($params);
     }
 } 
