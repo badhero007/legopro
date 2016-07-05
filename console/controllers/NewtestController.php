ac7649ad76b8c94ebff59a5acbbaf77c1da78489
376762626@qq.com <021271>
2016-07-05 18:17:57 +0800

    test ...

--- console/controllers/NewtestController.php
+++ console/controllers/NewtestController.php
@@ -20,7 +20,7 @@ class NewtestController extends Controller {
     public function actionHandleimg(){
         $path = \Yii::$app->getBasePath().'/../';
         $imgname = $path.'/frontend/web/img/14667387000007.jpg';
-        $im = @\imagecreatefromjpeg($imgname);
+        $im = @\imagecreatefrompng($imgname);
         //var_dump($im);exit();
         Helper::resizeImage($im,329,452,$path.'/frontend/web/img/th_14667387000007','.jpg');
     }
