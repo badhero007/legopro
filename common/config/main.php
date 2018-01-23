28be22e1812cd205bd4e969481a76fc07507afcc
376762626@qq.com <021271>
2018-01-23 14:06:42 +0800

    修改数据库密码

--- common/config/main.php
+++ common/config/main.php
@@ -13,22 +13,22 @@ return [
         'db' => [
             'class' => 'yii\db\Connection',
             'dsn' => 'mysql:host=127.0.0.1;dbname=mydb',
-            'username' => 'lego',
-            'password' => '021271',
+            'username' => 'root',
+            'password' => '021271!9',
             'charset' => 'utf8',
         ],
         'dianping' => [
             'class' => 'yii\db\Connection',
             'dsn' => 'mysql:host=127.0.0.1;dbname=bakdianping',
-            'username' => 'lego',
-            'password' => '021271',
+            'username' => 'root',
+            'password' => '021271!9',
             'charset' => 'utf8',
         ],
         'base'=>[
             'class' => 'yii\db\Connection',
             'dsn' => 'mysql:host=127.0.0.1;dbname=base',
-            'username' => 'lego',
-            'password' => '021271',
+            'username' => 'root',
+            'password' => '021271!9',
             'charset' => 'utf8',
         ],
 
